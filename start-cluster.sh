3e311b87fe5db6d1f8f3bcb22b007ab8500c79f5
Amith Koujalgi <amith.koujalgi@razorthink.com>
2020-09-22 14:39:35 +0530

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -5,6 +5,9 @@ IMG_NAME="amithkoujalgi/spark-with-hadoop-yarn-cluster"
 HOST_PREFIX="mycluster"
 NETWORK_NAME=$HOST_PREFIX
 
+# CLEANUP OLD CONTAINERS
+docker container prune -f
+
 N=${1:-2}
 NET_QUERY=$(docker network ls | grep -i $NETWORK_NAME)
 if [ -z "$NET_QUERY" ]; then
@@ -17,7 +20,7 @@ i=1
 while [ $i -le $N ]
 do
 	HADOOP_SLAVE="$HOST_PREFIX"-slave-$i
-	
+
 	docker run --name $HADOOP_SLAVE -h $HADOOP_SLAVE --net=$NETWORK_NAME -itd "$IMG_NAME"
 	i=$(( $i + 1 ))
 done
