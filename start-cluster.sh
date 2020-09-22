89cc7cd74b9d48f5fc286b6f2571ac4643450640
Amith Koujalgi <amith.koujalgi@razorthink.com>
2020-09-22 16:24:57 +0530

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -23,7 +23,7 @@ while [ $i -le $N ]
 do
 	HADOOP_SLAVE="$HOST_PREFIX"-slave-$i
 
-	docker run --name $HADOOP_SLAVE -h $HADOOP_SLAVE --net=$NETWORK_NAME -itd "$IMG_NAME"
+	docker run -v ~/:/app --name $HADOOP_SLAVE -h $HADOOP_SLAVE --net=$NETWORK_NAME -itd "$IMG_NAME"
 	i=$(( $i + 1 ))
 done
 
@@ -31,7 +31,7 @@ done
 
 HADOOP_MASTER="$HOST_PREFIX"-master
 
-docker run --name $HADOOP_MASTER \
+docker run -v ~/:/app --name $HADOOP_MASTER \
 		-h $HADOOP_MASTER \
 		--net=$NETWORK_NAME \
 		-p 8088:8088 \
